d5b6f201b7615d8628287e87956cb66a50765769
Aleksandr <nithogg@mail.ru>
2021-02-12 17:00:43 +0300

    stage 3-5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,52 @@
 export default class NotificationMessage {
 
+  constructor(note, {
+    duration = 0,
+    type = ''
+  } = {}) {
+    this.duration = duration;
+    this.type = type;
+    this.note = note;
+    this.render();
+  }
+
+  get template() {
+    let value = (this.duration / 1000) + 's';
+    return `
+  <div class="notification ${this.type}" style="--value: ${value}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.note}
+      </div>
+    </div>
+  </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show() {
+    document.querySelectorAll('.notification').forEach((a) => a.remove());
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+    document.body.append(element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element.remove();
+    this.element = null;
+  }
 }
+
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,93 @@
 export default class SortableTable {
 
+  sortedData = [];
+
+  constructor(
+      header = [],
+    {
+      data = []
+    } = {}) {
+
+    this.header = header;
+    this.data = data;
+    this.sortedData = data;
+    this.render();
+  }
+
+  getHeader(header) {
+    return header
+      .map(item => {
+        return `<div class="sortable-table__cell" data-id="${item.id}" data-sortable="false" data-order="asc">
+        <span>${item.title}</span>
+      </div>`;
+      })
+      .join('');
+  }
+
+  getRows(header, oneI) {
+    return header
+      .map(item => {
+        if (item.id === 'images') {
+          return `<div class="sortable-table__cell">
+          <img class="sortable-table-image" alt="Image" src="${oneI.images[0].url}">
+        </div>`;
+        } else {
+          return `<div class="sortable-table__cell">${oneI[item.id]}</div>`;
+
+        }
+      })
+      .join('');
+  }
+
+
+  getBody(header, data) {
+    return data
+      .map(item => {
+        return `<a href="" class="sortable-table__row">
+         ${this.getRows(header, item)}
+         </a>`;}).join('');
+  }
+
+  get template() {
+    return `
+<div data-element="productsContainer" class="products-list__container">
+  <div class="sortable-table">
+    <div data-element="header" class="sortable-table__header sortable-table__row">
+     ${this.getHeader(this.header)}
+     </div>
+     <div data-element="body" class="sortable-table__body">
+          ${this.getBody(this.header, this.sortedData)}
+    </div>
+  </div>
+</div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+    document.body.append(element);
+  }
+
+  sort(field, order) {
+    this.remove();
+    let arr;
+    if (typeof this.sortedData[0][field] === "number") {
+      arr = [...this.sortedData].sort((prev, next) => prev[field] - next[field]);
+    } else {
+      arr = [...this.sortedData].sort((prev, next) => ('' + prev[field]).localeCompare(next[field]));
+    }
+    this.sortedData = order === 'asc' ? arr : arr.reverse();
+    this.render();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
 
